1612602f533c08a014abc4a9d0a0df915bf337ae
novakvova <novakvova@gmail.com>
2022-01-24 11:38:23 +0200

    Add role and show list

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -5,20 +5,24 @@ import org.hibernate.query.Query;
 import utils.HibernateSessionFactoryUtil;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-//        Role role = new Role();
-//        role.setName("admin");
-//        session.save(role);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter role name:");
+        Role role = new Role();
+        role.setName(in.nextLine());
+        session.save(role);
 //
 //        Role role2 = new Role();
 //        role2.setName("student");
 //        session.save(role2);
         Query query = session.createQuery("FROM Role");
         List<Role> roles = query.list();
-        for (Object role : roles)
+        System.out.println("Roles list: ");
+        for (Object item : roles)
             System.out.println(role);
         session.close();
     }
