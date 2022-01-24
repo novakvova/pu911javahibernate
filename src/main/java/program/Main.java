fa2f9e335948e20567a275db6f00577a2f3e1d13
novakvova <novakvova@gmail.com>
2022-01-24 11:48:49 +0200

    add utf-8

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in, "UTF-8");
         System.out.println("Enter role name:");
         Role role = new Role();
         role.setName(in.nextLine());
